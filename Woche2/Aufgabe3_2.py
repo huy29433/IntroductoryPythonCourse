30b230be3f79ac19a279ca75c559e28598da6c6a
huy29433 <64016327+huy29433@users.noreply.github.com>
2020-11-17 17:50:11 +0100

    Update Aufgabe3_2.py

--- Woche2/Aufgabe3_2.py
+++ Woche2/Aufgabe3_2.py
@@ -1,6 +1,7 @@
 def func(x):
     return x ** 2 + 3 * x - 5 == 0
-
+# nicht die allerbeste Wahl für den Funktionennamen, da man annehmen könnte, dass das eine Funktion im mathematischen Sinne ist.
+# besser: solves_equation
 
 equation = "x^2 + 3*x - 5 = 0"
 
@@ -9,8 +10,9 @@ val_for_x = float(input("Welche Zahl soll geprueft werden?"))
 
 result_string = "Ihre Eingabe ist "
 if not func(val_for_x):
-    result_string += "k"
+    result_string += "k" # :D
 
 result_string += f"eine Loesung zu {equation}"
 
 print(result_string)
+# schön!
