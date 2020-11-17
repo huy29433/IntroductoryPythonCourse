8783b32faa6a4d2c0e3a9807a58146297ab451d4
huy29433 <64016327+huy29433@users.noreply.github.com>
2020-11-17 17:47:05 +0100

    Update Aufgabe2.py

--- Woche2/Aufgabe2.py
+++ Woche2/Aufgabe2.py
@@ -1,3 +1,6 @@
-approx_pi = 22/7
+approx_pi = 22/7 # :D
 
 print(f"{approx_pi:.4}")
+# besser:
+print(f"{approx_pi:.3f}")
+# Auf diese Weise hast du genau drei Nachkommastellen, egal wie groß der ganzzahlige Teil deiner Zahl ist
