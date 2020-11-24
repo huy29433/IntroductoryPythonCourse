d7f6237bd3549e40a26552e1786f87c36b7008fb
DeathFaller <40919244+DeathFaller@users.noreply.github.com>
2020-11-24 09:08:00 +0100

    Update Aufgabe_6.py
    Added comments with

--- Woche3/Aufgabe_6.py
+++ Woche3/Aufgabe_6.py
@@ -1,15 +1,30 @@
 def main():
-    A = []
-    A.append(A)
-    A.append(2)
 
-    print(A)
-    print(len(A))
-    print(len(A[0]))
-    print(len(A[0][0]))
+    # Inhalt:   # ----  # ----  # ----  # ----  # ----  # ----  #
+    # Adresse:  # 0x00  # 0x01  # 0x02  # 0x03  # 0x04  # 0x05  #
+    #
+    #
+    A = []              # A enthält Adresse (z.B. 0x01)
+    # Inhalt:   # ----  # []  # ----  # ----  # ----  # ----  #
+    # Adresse:  # 0x00  # 0x01  # 0x02  # 0x03  # 0x04  # 0x05  #
+    A.append(A)         # Der Liste wird Adresse von A angehängt
 
-    A[0][0] = 1
-    print(A)
+    # Inhalt:   # ----  #[0x01] # ----  # ----  # ----  # ----  #
+    # Adresse:  # 0x00  # 0x01  # 0x02  # 0x03  # 0x04  # 0x05  #
+
+    print(A)            # Auflösung der Elemente klappt nicht, da immer wieder auf Adresse 0x01 geschaut wird
+    print(len(A))       # Die Liste ist aber immer noch nur 1 Element lang, da die Liste eine (besser gesagt: sich
+                        # selbst als) Liste (usw.) enthält
+    print(len(A[0]))    # Auch das erste Element der Liste A ist nur 1 lang, da diese auch wiederum eine Liste einer
+                        # Liste (...) enthält
+    print(len(A[0][0])) # Auch das erste Element der Liste A ist nur 1 lang, da diese auch wiederum eine Liste einer
+                        # Liste (...) enthält
+
+    A[0][0] = 1         # Gehe in Element 0 der Adresse 0x01 (->0x01), gehe auch dort in Element 0 (->0x01)
+                        # Setze dieses Element auf 1
+            # Inhalt:   # ----  #  [1]  # ----  # ----  # ----  # ----  #
+    # Adresse:  # 0x00  # 0x01  # 0x02  # 0x03  # 0x04  # 0x05  #
+    print(A)            # Referenzierung auf sich selbst gelöst, nur noch 1 in Liste enthalten
 
 
 if __name__ == "__main__":
